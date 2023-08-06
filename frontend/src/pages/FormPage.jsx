441627915c9788985a10dcfe55b64199976a2e6b
siriusoya <siriusoya@gmail.com>
2023-08-06 11:30:22 +0700

    created mvc on server side and created routes on client side

---
+++ frontend/src/pages/FormPage.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { TextField, MaskedTextField } from "@fluentui/react/lib/TextField";
+import { Stack } from "@fluentui/react/lib/Stack";
+
+const stackTokens = { childrenGap: 50 };
+const iconProps = { iconName: "Calendar" };
+const stackStyles = { root: { width: 650 } };
+const columnProps = {
+  tokens: { childrenGap: 15 },
+  styles: { root: { width: 300 } },
+};
+
+function FormPage() {
+  return (
+    <>
+      <Stack>
+        <TextField label="Standard" />
+      </Stack>
+
+      <Stack horizontal tokens={stackTokens} styles={stackStyles}>
+        <Stack {...columnProps}>
+          <TextField label="Standard" />
+          <TextField label="Disabled" disabled defaultValue="I am disabled" />
+          <TextField label="Read-only" readOnly defaultValue="I am read-only" />
+          <TextField label="Required " required />
+          <TextField ariaLabel="Required without visible label" required />
+          <TextField label="With error message" errorMessage="Error message" />
+        </Stack>
+        <Stack {...columnProps}>
+          <MaskedTextField
+            label="With input mask"
+            mask="m\ask: (999) 999 - 9999"
+            title="A 10 digit number"
+          />
+          <TextField label="With an icon" iconProps={iconProps} />
+          <TextField
+            label="With placeholder"
+            placeholder="Please enter text here"
+          />
+          <TextField
+            label="Disabled with placeholder"
+            disabled
+            placeholder="I am disabled"
+          />
+          <TextField
+            label="Password with reveal button"
+            type="password"
+            canRevealPassword
+            revealPasswordAriaLabel="Show password"
+          />
+        </Stack>
+      </Stack>
+    </>
+  );
+}
+
+export default FormPage;
---
+++ frontend/src/pages/TaskListPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function TaskListPage() {
+  return (
+    <>
+      
+    </>
+  );
+}
+
+export default TaskListPage;
